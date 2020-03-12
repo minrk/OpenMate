847a5a451f21f1290f462a0a4cb46f8f64852cb7
Min RK <benjaminrk@gmail.com>
2020-03-12 21:59:39 +0100

    use reveal_in_sidebar to highlight directories
    no real api for directories, so we have to fake it with files
    
    also make the single orphan window optional and off by default. Not sure if that's good or not.

--- openmate.py
+++ openmate.py
@@ -8,13 +8,18 @@ If no such window exists, open a new window with the parent folder.
 """
 
 import os
+import shutil
 
 import sublime
 import sublime_plugin
 
+package_settings = sublime.load_settings("OpenMate.sublime-settings")
+single_orphan_window = package_settings.get("single-orphan-window", False)
+
 
 class OpenMateCommand(sublime_plugin.ApplicationCommand):
     """Open a file or folder in an existing window if it is already open"""
+
     def run(self, path):
         # prioritize current window
         window_list = sublime.windows()
@@ -38,15 +43,48 @@ class OpenMateCommand(sublime_plugin.ApplicationCommand):
             for folder in win.folders():
                 if (path + os.path.sep).startswith(folder + os.path.sep):
                     if not os.path.isdir(path):
+                        # create file
+                        with open(path, "w"):
+                            pass
                         win.open_file(path)
+                        win.run_command("reveal_in_side_bar")
+                    else:
+                        # it's a directory. No way to reveal a folder in side bar,
+                        # so reveal the first file in the side bar
+                        found = False
+                        for root, dirs, files in os.walk(path):
+                            for filename in sorted(files):
+                                # check if there's already an open file
+                                # in our folder and focus it
+                                file_path = os.path.join(root, filename)
+                                view = win.find_open_file(file_path)
+                                if view:
+                                    win.focus_view(view)
+                                    win.run_command("reveal_in_side_bar")
+                                    found = True
+                                    break
+                            if found:
+                                break
+                            if files:
+                                # files found in folder, but none open
+                                # open first one, reveal in sidebar
+                                # and close immediately
+                                file_path = os.path.join(root, sorted(files)[0])
+                                win.open_file(file_path)
+                                win.run_command("reveal_in_side_bar")
+                                win.run_command("close")
+                                found = True
+                        if not found:
+                            print("no file to open for folder %s" % path)
+
                     if win is not active_window:
                         self.focus(win)
                     return
 
         # not already open, open in first no-folder window
-        # open all orphan files
-        if not its_a_dir:
-            folderless_windows = [ w for w in window_list if not w.folders() ]
+        # if single-orphan-window is enabled
+        if single_orphan_window and not its_a_dir:
+            folderless_windows = [w for w in window_list if not w.folders()]
             if folderless_windows:
                 win = folderless_windows[0]
                 win.open_file(path)
@@ -58,20 +96,23 @@ class OpenMateCommand(sublime_plugin.ApplicationCommand):
         sublime.run_command("new_window")
         win = sublime.active_window()
         if its_a_dir:
-            proj = win.project_data() or {'folders': []}
+            # TODO: find window with orphaned files and add folder to it
             dir_path = path
-            proj['folders'].append({'path': dir_path})
+            proj = win.project_data() or {"folders": []}
+            proj["folders"].append({"path": dir_path})
             win.set_project_data(proj)
         else:
             win.open_file(path)
 
     def nearby_window_key(self, path, win):
         """sort-key function for the window with files in the nearest folder"""
-        distance = float('inf')
+        distance = float("inf")
         for view in win.views():
             filename = view.file_name()
             if filename:
-                distance = min(self.path_distance(path, os.path.dirname(filename)), distance)
+                distance = min(
+                    self.path_distance(path, os.path.dirname(filename)), distance
+                )
         return distance
 
     def path_distance(self, a, b):
@@ -87,11 +128,10 @@ class OpenMateCommand(sublime_plugin.ApplicationCommand):
             # since the results are symmetrical
             a_parts, b_parts = b_parts, a_parts
         for prefix_len in range(len(a_parts) + 1):
-            if a_parts[:prefix_len + 1] != b_parts[:prefix_len + 1]:
+            if a_parts[: prefix_len + 1] != b_parts[: prefix_len + 1]:
                 break
 
         return len(a_parts) + len(b_parts) - (2 * prefix_len)
-
 
     # window-focus logic from https://github.com/ccampbell/sublime-goto-window
 
@@ -107,26 +147,32 @@ class OpenMateCommand(sublime_plugin.ApplicationCommand):
         # work on Windows
         if active_view is not None:
             window_to_move_to.focus_view(active_view)
-            window_to_move_to.run_command('focus_neighboring_group')
+            window_to_move_to.run_command("focus_neighboring_group")
             window_to_move_to.focus_view(active_view)
             return
 
         if active_group is not None:
             window_to_move_to.focus_group(active_group)
-            window_to_move_to.run_command('focus_neighboring_group')
+            window_to_move_to.run_command("focus_neighboring_group")
             window_to_move_to.focus_group(active_group)
 
     def description(self):
         return "Open a file in the right window"
 
+
 class InstallOpenMateCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         win = sublime.active_window()
-        win.show_input_panel("Select installation location", "/usr/local/bin/openmate", self.on_done, None, None)
+        win.show_input_panel(
+            "Select installation location",
+            "/usr/local/bin/openmate",
+            self.on_done,
+            None,
+            None,
+        )
 
     def on_done(self, dest):
-        src = os.path.join(os.path.dirname(__file__), 'mate')
-        import shutil
+        src = os.path.join(os.path.dirname(__file__), "mate")
         shutil.copyfile(src, dest)
 
     def description(self):
